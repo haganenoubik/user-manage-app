ef5125d5ec7455bcce2d722b0b663138ef76469d
haganenoubik <haganenoubik@gmail.com>
2024-06-09 22:54:45 +0900

    Chakra UIのグローバルなスタイルの適用

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
 import { Button, ChakraProvider } from '@chakra-ui/react'
+import theme from "./theme/theme";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme} >
       <App />
       <Button colorScheme='purple' variant='outline'>Button</Button>
     </ChakraProvider>
