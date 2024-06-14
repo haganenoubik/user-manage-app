7d40eb89a352b7a5ff0c3d487d7c72e8c626686b
haganenoubik <haganenoubik@gmail.com>
2024-06-14 17:01:17 +0900

    メニューボタンとサイドメニューのコンポーネント化

---
+++ src/components/molecules/MenuDrawer.tsx
@@ -0,0 +1,31 @@
+import { FC, memo } from 'react';
+import { Button, Drawer, DrawerBody, DrawerContent, DrawerOverlay } from '@chakra-ui/react';
+
+type Props = {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+export const MenuDrawer: FC<Props> = memo((props) => {
+  const { isOpen, onClose } = props;
+  return (
+    <>
+      <Drawer
+        isOpen={isOpen}
+        placement="left"
+        size="xs"
+        onClose={onClose}
+      >
+        <DrawerOverlay>
+          <DrawerContent>
+            <DrawerBody p={0} bg="gray.100">
+              <Button w="100%">TOP</Button>
+              <Button w="100%">ユーザー一覧</Button>
+              <Button w="100%">設定</Button>
+            </DrawerBody>
+          </DrawerContent>
+        </DrawerOverlay>
+      </Drawer>
+    </>
+  )
+});
