fb96cc072405c51a7628ded1368bf78d8fff434e
haganenoubik <haganenoubik@gmail.com>
2024-06-15 17:42:38 +0900

    ログインユーザー情報をContextを使用してグローバルに参照できるようにする

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -6,18 +6,22 @@ import { UserManagement } from '../components/pages/UserManagement';
 import { Setting } from '../components/pages/Setting';
 import { Page404 } from '../components/pages/Page404';
 import { HeaderLayout } from '../components/templates/HeaderLayout';
+import { LoginUserProvider } from '../providers/LoginUserProvider';
 
 UserManagement
 export const Router: FC = memo(() => {
   return (
-    <Routes>
-      <Route path='/' element={<Login />} />
-      <Route path='home' element={<HeaderLayout />}>
-        <Route index element={<Home />} />
-        <Route path='user_management' element={<UserManagement />} />
-        <Route path='setting' element={<Setting />} />
-        <Route path='*' element={<Page404 />} />
-      </Route>
-    </Routes>
-  )
+    <LoginUserProvider>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="home" element={<HeaderLayout />}>
+          <Route index element={<Home />} />
+          <Route path="user_management" element={<UserManagement />} />
+          <Route path="setting" element={<Setting />} />
+          <Route path="*" element={<Page404 />} />
+        </Route>
+        <Route path="*" element={<Page404 />} />
+      </Routes>
+    </LoginUserProvider>
+  );
 });
